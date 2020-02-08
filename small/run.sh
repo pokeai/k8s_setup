f1b899aaddb99cef27091d8b1d5e1d8547b342c5
cpchung <chakpongchung@gmail.com>
2020-02-08 16:32:15 -0500

    gitignore

--- small/run.sh
+++ small/run.sh
@@ -1,22 +1,56 @@
-spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class SimpleApp --master local[*] target/small-1.0-SNAPSHOT.jar
+# spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class SimpleApp --master local[*] target/small-1.0-SNAPSHOT.jar
+# spark-submit --class SimpleApp --master local[*] target/small-1.0-SNAPSHOT.jar
+eval $(minikube docker-env)
 
-spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class SimpleApp \
---master "k8s://https://192.168.99.101:8443" \
+
+spark-submit --class SimpleApp \
+--master "k8s://https://192.168.39.146:8443" \
 --deploy-mode cluster \
---conf spark.kubernetes.container.image=bonbuild:newtag \
+--conf spark.kubernetes.container.image=cpchung:latest \
 --conf spark.kubernetes.image.pullPolicy=IfNotPresent \
---conf spark.executor.instances=5 \
-target/small-1.0-SNAPSHOT.jar
+--conf spark.executor.instances=1 \
+--conf spark.kubernetes.authenticate.driver.serviceAccountName=default \
+local:///small.jar
+
+#target/small-1.0-SNAPSHOT.jar
+# --conf spark.kubernetes.container.image=localhost:5000/cpchung:latest \
+
+# spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class SimpleApp \
+# --master "k8s://https://192.168.99.101:8443" \
+# --deploy-mode cluster \
+# --conf spark.kubernetes.container.image=bonbuild:newtag \
+# --conf spark.kubernetes.image.pullPolicy=IfNotPresent \
+# --conf spark.executor.instances=5 \
+# target/small-1.0-SNAPSHOT.jar
 
 # build docker base image
-cd spark-2.4.4-bin-hadoop2.7
-bin/docker-image-tool.sh -t bon build
+# cd spark-2.4.4-bin-hadoop2.7
+# bin/docker-image-tool.sh -t bon build
 
 # docker images
 # docker build -t repositoryName:optionalTag .
 # docker build --help
 
 # create/run container from image
-docker run <image_id>
-docker run <repositoryName>:<tagName>
+# docker run <image_id>
+# docker run <repositoryName>:<tagName>
 
+# kubectl delete pods
+
+
+# kubectl cluster-info
+
+#kubectl delete pods --all
+
+#kubectl logs simpleapp-1580782281051-driver
+
+#docker image prune --all --filter until=48h
+
+# kubectl create serviceaccount spark
+
+#minikube docker-env
+
+#minikube start --kubernetes-version v1.12.0
+
+#https://stackoverflow.com/questions/47973570/kubernetes-log-user-systemserviceaccountdefaultdefault-cannot-get-services
+#https://github.com/fabric8io/fabric8/issues/6840
--- small/src/main/java/SimpleApp.java
+++ small/src/main/java/SimpleApp.java
@@ -5,8 +5,8 @@ import org.apache.spark.sql.SparkSession;
 public class SimpleApp {
     public static void main(String[] args) {
         String logFile = "README.md"; // Should be some file on your system
-//        SparkSession spark = SparkSession.builder().master("local[*]").appName("Simple Application").getOrCreate();
-        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+        SparkSession spark = SparkSession.builder().master("local[*]").appName("Simple Application").getOrCreate();
+//        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
         Dataset<String> logData = spark.read().textFile(logFile).cache();
 
         long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
@@ -15,5 +15,6 @@ public class SimpleApp {
         System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
 
         spark.stop();
+        System.exit(0);
     }
 }
